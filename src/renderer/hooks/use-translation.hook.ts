b24b079f88137a4c5dfc97a6aa38933f2d155c4e
Gaëtan <gaetangerard@hotmail.fr>
2022-11-14 15:45:55 +0100

    add hook

---
+++ src/renderer/hooks/use-translation.hook.ts
@@ -0,0 +1,12 @@
+import { I18nService } from "renderer/services/i18n.service"
+
+export function useTranslation(): (translationKey: string, args?: Record<string, string>) => string{
+   
+   const i18nService = I18nService.getInstance();
+
+   return (key: string, args?: Record<string, string>) => {
+      if(!key){ return key; }
+      const tranlatables = key.split(" ");
+      return tranlatables.map((key) => i18nService.translate(key, args)).join(" ");
+   }
+}
