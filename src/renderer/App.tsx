00acb66fc160951a77f8772251c00905c571dc0a
Gaëtan <gaetangerard@hotmail.fr>
2022-11-19 12:05:15 +0100

    Translation add

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,20 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import "tailwindcss/tailwind.css";
+import { useTranslation } from "renderer/hooks/use-translation.hook";
+
+
 
 const Hello = () => {
+
+  const t = useTranslation();
+
+
   return (
     <div className="h-screen flex items-center justify-center bg-gray-200">
       <h1 className="text-blue-500">Hello Tailwind</h1>
+      <h2>Amazing translation :</h2>
+      <h2>{t("NavBar.news")}</h2>
     </div>
   );
 };
