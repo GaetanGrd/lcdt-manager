9084acbe70d53f76ec3e3fa84ea9bc8dea526e70
Gaëtan <gaetangerard@hotmail.fr>
2022-12-13 00:26:28 +0100

    add button component for nav bar

---
+++ src/renderer/components/nav-bar/nav-bar-button.component.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from 'renderer/hooks/use-translation.hook';
+import {Link} from 'react-router-dom';
+
+type Props = {              // Exemple de valeur :
+  txt : string,             // "pages.game.title"
+  linkTo : string,            // /home
+}
+
+export function NavBarButton({txt, linkTo} : Props ) {
+  const translate = useTranslation();
+  const className = "hover:bg-[#363949] rounded py-2 inline-block text-white relative w-auto px-4";
+
+
+  return (
+    <Link to={linkTo} className={className} >{translate(txt)}</Link>
+  )
+}
